60f5688e7ae3d96e6bd3cf97cbe32015e3c9ced5
BOUSBA-nadir <56542060+BOUSBA-nadir@users.noreply.github.com>
2019-11-24 05:15:54 +0100

    Add files via upload

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function validation () {
   this.event.preventDefault();
   var nom = document.getElementById('Nom');
@@ -33,39 +33,3 @@ if (error ==="") {
 
 }
 }
-=======
-function validation () {
-  this.event.preventDefault();
-  var nom = document.getElementById('Nom');
-  var prenom = document.getElementById('prénom');
-  var date = document.getElementById('date');
-  var adresse= document.getElementById('addr');
-  var mail = document.getElementById('Mail');
-  var errorText = document.getElementById("error");
-  var resultText = document.getElementById("resultat");
-   var error = "";
-   var resultat= "";
- var list = document.querySelectorAll("input");
- list.forEach(function (item) {
-  if (item.value.length<5){
-    error = "les champs doivent contenir au moins cinq caracteres";
-  }
-
-
-
-
-});
-
-
-if (error ==="") {
-  resultat = "bienvenue" + " "+nom.value + " " +prenom.value;
-  resultText.innerText=resultat;
-}else {
-  errorText.innerText=error;
-
-
-
-
-}
-}
->>>>>>> 4adb4915e4d1df79d53aae9f21df835e436b9717
